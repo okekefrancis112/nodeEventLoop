ef4a2c72b5c770038f8e71f2d4cb52a5a98b66f6
Francis Chukwudalu Okeke <okeke98@gmail.com>
2023-01-07 18:39:51 +0100

    All About Node Event Loop

---
+++ main/loop.js
@@ -0,0 +1,36 @@
+// node myFile.js
+
+myFile.runContents();
+
+const pendingTimers = [];
+const pendingOSTasks = [];
+const pendingOperations = [];
+
+// New timers, tasks, operations are recorded from myFIle running
+function shouldContinue() {
+// check one: Any pending setTimeout, setInterval, setImmediate?
+// check two: Any pending OS tasks? (Like server listening to port)
+// check three: Any pending long running operations? (Like fs module)
+
+    return pendingTimers.length || pendingOSTasks.length || pendingOperations.length;
+}
+
+// Entire body executes in one 'tick
+while (shouldContinue()) {
+    //  1) Node looks at pendingTimers and sees
+    //      if any functions are ready to be called.(setTimeout, setInterval)
+
+    //  2) Node looks at pendingOSTasks and pendingOperations and calls relevant callbacks.
+
+    //  3) Pause execution, Continue when...
+    //  - a new pendingOSTask is done
+    //  - a new pendingOperation is done
+    //  - a timer is about to complete
+
+    //  4) Look at pendingTimers. Call any setImmediate.
+
+    //  5) Handle any 'close' events.
+
+}
+
+// exit back to terminal

ffc23d63cf1648658d649b2df7cadd50400ed760
Francis Chukwudalu Okeke <okeke98@gmail.com>
2023-01-09 02:38:25 +0100

    working with cluster

--- main/index.js
+++ main/index.js
@@ -1,8 +1,27 @@
-const express = require('express');
-const app = express();
+const cluster = require('cluster');
 
-app.get('/', (req, res) => {
-    res.send('Hi there');
-});
+// console.log(cluster.isMaster);
 
-app.listen(3000);
+// Is the file being executed in master mode?
+if (cluster.isMaster) {
+    // Cause index.js to be executed *again* in child mode.
+    cluster.fork();
+} else {
+    // I'm a child, i'm going to act as a server and do nothing else.
+        const express = require('express');
+        const app = express();
+
+
+        function doWork(duration) {
+            const start = Date.now();
+            while(Date.now() - start < duration) {}
+        }
+
+        // server
+        app.get('/', (req, res) => {
+            doWork(5000);
+            res.send('Hi there');
+        });
+
+        app.listen(3000);
+    }
